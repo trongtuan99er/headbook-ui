b94aab55cc83c7a76b091e69bf69bf6a30cb455a
trongtuan99er <trongtuan681@gmail.com>
2023-03-16 16:44:34 +0700

    add navbar ui

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,9 +1,43 @@
-import './navBar.scss'
+import "./navBar.scss";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from "react-router-dom";
 
-const NavBar = () => {
+const Navbar = () => {
+
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>Headbook</span>
+        </Link>
+        <HomeOutlinedIcon />
+        <WbSunnyOutlinedIcon/>
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNAUvIj8tIlcc6MemlkLaXGlOLNplzf-3euA&usqp=CAU"
+            alt="avatar"
+          />
+          <span>TrongTuan</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default Navbar;
