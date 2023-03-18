f39e4d8c6c54d5100460c13dcd97d2bbdc9a4228
trongtuan99er <trongtuan681@gmail.com>
2023-03-18 13:33:18 +0700

    darkmode with context API

--- src/App.js
+++ src/App.js
@@ -12,13 +12,15 @@ import Profile from './pages/profile/Profile';
 import NavBar from './components/navBar/NavBar';
 import LeftBar from './components/leftBar/LeftBar';
 import RightBar from './components/rightBar/RightBar';
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
 
 function App() {
   const currentUser = true
-
+  const { darkMode } = useContext(DarkModeContext);
   const Layout = () => {
     return (
-      <div>
+      <div className={`theme-${darkMode ? "dark" : "light"}`}>
         <NavBar />
         <div style={{ display: "flex" }}>
           <LeftBar />
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -6,10 +6,13 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { DarkModeContext } from "../../context/darkModeContext";
 
 const Navbar = () => {
-
+  const { toggle, darkMode } = useContext(DarkModeContext);
   return (
     <div className="navbar">
       <div className="left">
@@ -17,7 +20,11 @@ const Navbar = () => {
           <span>Headbook</span>
         </Link>
         <HomeOutlinedIcon />
-        <WbSunnyOutlinedIcon/>
+        {darkMode ? (
+          <WbSunnyOutlinedIcon onClick={toggle} />
+        ) : (
+          <DarkModeOutlinedIcon onClick={toggle} />
+        )}
         <GridViewOutlinedIcon />
         <div className="search">
           <SearchOutlinedIcon />
