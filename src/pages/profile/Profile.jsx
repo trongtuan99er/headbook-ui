f0ec296faa7fc197e211394c93e1008ab614387f
trongtuan99er <trongtuan681@gmail.com>
2023-03-24 10:06:24 +0700

    handle follow user

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -7,7 +7,7 @@ import Posts from "../../components/posts/Posts"
 import { useContext, useState } from "react";
 import { AuthContext } from "../../context/authContext";
 import { useLocation, Link } from "react-router-dom";
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { makeRequest } from "../../callAPI";
 import ProfileDetail from "../profileDetai/ProfileDetail";
 
@@ -15,7 +15,9 @@ const Profile = () => {
   const [open, setOpen] = useState(false)
   const [showDetail, setShowDetail] = useState(false)
   const { currentUser } = useContext(AuthContext)
+  const queryClient = useQueryClient()
   const userId = parseInt(useLocation().pathname.split("/")[2])
+
   const { isLoading, error, data } = useQuery({
     queryKey: ["user"],
     queryFn: () => makeRequest.get("/users/find/"+ userId).then(res => {
@@ -23,6 +25,28 @@ const Profile = () => {
     })
     }
   )
+  const { loading: rlIsLoading, data: dataRelationship } = useQuery({
+    queryKey: ["relationship"],
+    queryFn: () => makeRequest.get("/relationships?followedUserId="+ userId).then(res => {
+      return res.data
+    })
+    }
+  )
+  
+  const mutation = useMutation((following) => {
+    if (following) return makeRequest.delete("/relationships?userId="+ userId)
+    return makeRequest.post("/relationships", {userId})
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['relationship'])
+    }
+  })
+
+  const handleFollow = (e) => {
+    e.preventDefault()
+    mutation.mutate(dataRelationship?.includes(currentUser.id))
+  }
+
   const handleOpen = () => {
     setOpen(!open)
   }
@@ -50,7 +74,7 @@ const Profile = () => {
               />
             <span>{data?.name}</span>
             </div>
-            {userId === currentUser.id ? <button>Cập nhập</button> : <button>Theo dỗi</button>}
+            {rlIsLoading ? <button>Loading</button> : userId === currentUser.id ? <button>Cập nhập</button> : <button onClick={handleFollow}>{dataRelationship?.includes(currentUser.id) ? "Đang theo dõi" : "Theo dỗi"}</button>}
             
           </div>
           <div className="moreInfo">
